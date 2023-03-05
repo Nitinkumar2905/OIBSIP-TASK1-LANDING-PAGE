46564c16831211a2765533f18de02ef8ede0af39
nitin <nimble2905@gmail.com>
2023-03-05 14:00:19 +0530

    project setup

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let x = document.querySelector(".MyDiv");
+    let y = document.querySelector(".My-Menu");
+    let z = document.querySelector(".My-Close");
+    let n = document.querySelector(".nav-bar");
+        x.classList.toggle("mobile-menu");
+        y.classList.toggle("menu");
+        z.classList.toggle("close");
+        n.classList.toggle("nav-border");
+        }
